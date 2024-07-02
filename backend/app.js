1ce07538d86710ec76ac16107a5cf818171de8e8
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2024-07-03 02:01:07 +0430

    create sign up Api

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,16 @@
 import express from "express"
 import morgan from "morgan"
 
+import authRouter from "./routes/auth.route.js"
+
 const app = express()
 
 app.use(morgan("dev"))
 app.use(express.json())
+
+// Routes
+// app.use("/api/user", userRouter)
+app.use("/api/auth", authRouter)
 
 
 export default app
