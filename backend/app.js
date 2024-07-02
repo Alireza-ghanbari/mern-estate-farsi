4ec56c9ae2058ac9f5f31bd7b74955548703f4fc
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2024-07-03 02:07:44 +0430

    handle errors

--- backend/app.js
+++ backend/app.js
@@ -12,5 +12,14 @@ app.use(express.json())
 // app.use("/api/user", userRouter)
 app.use("/api/auth", authRouter)
 
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500
+    const message = err.message || "Interna; Server Error"
+    return res.status(statusCode).json({
+        statusCode,
+        message
+    })
+})
+
 
 export default app
