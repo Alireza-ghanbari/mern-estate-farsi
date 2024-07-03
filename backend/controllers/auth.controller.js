c898b9cb80922e4d39fabb2c00c7c16485bec0e1
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2024-07-03 14:15:31 +0430

    create sign-in api

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -15,14 +17,13 @@ export const signup = async (req, res, next) => {
       error: "با این نام کاربری حساب ساخته شده است",
     });
   }
-  
+
   const existingEmail = await User.findOne({ email });
   if (existingEmail) {
     return res.status(400).json({
       error: "با این ایمیل حساب ساخته شده است",
     });
   }
-
 
   if (password.length < 4) {
     return res.status(400).json({
@@ -40,6 +41,23 @@ export const signup = async (req, res, next) => {
   }
 };
 
-export const signin = async (req, res, next) => {};
+export const signin = async (req, res, next) => {
+  const { username, password } = req.body;
+  try {
+    const validUser = await User.findOne({ username });
+    if (!validUser) return next(errorHandler(404, "حساب پیدا نشد"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword)
+      return next(errorHandler(401, "گذروارژه یا نام کاربری اشتباه است"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pw, ...userInfo } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(userInfo);
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const signOut = async (req, res, next) => {};
