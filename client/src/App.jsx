95b812e5bc479368e270f6eadc9e1d9b5b016873
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2024-07-03 17:20:21 +0430

    update navbar and create profile page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate, useNavigate } from "react-router-dom";
 
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
@@ -6,17 +6,22 @@ import SignUp from "./pages/SignUp";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Navbar from "./components/Navbar";
+import PrivateRoute from "./components/PrivateRoute";
+import { useSelector } from "react-redux";
 
 export default function App() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={currentUser? <Navigate to={"/"}/> : <SignIn/>} />
+        <Route path="/sign-up" element={currentUser? <Navigate to={"/"}/> : <SignUp />} />
         <Route path="/about" element={<About />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,54 +1,49 @@
 import { FaHome, FaSearch, FaUser } from "react-icons/fa";
 import { FaLayerGroup } from "react-icons/fa6";
 import { Link } from "react-router-dom";
-import { IoMdMore } from "react-icons/io";
-
+import { useSelector } from "react-redux";
 
 export default function Navbar() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-7xl mx-auto py-3 px-2 sm:px-8">
-        <ul className="sm:flex gap-12 items-center hidden text-lg">
-          <Link to={"/sign-in"}>
-            <li className="text-slate-700 hover:border-b border-black hover:text-black">
-              ورود
-            </li>
-          </Link>
-          <Link to={"/about"}>
-            {" "}
-            <li className="text-slate-700 hover:border-b border-black hover:text-black">
-            
-              درباره
-            </li>
-          </Link>
-          <Link to={"/"}>
-            <li className="text-slate-700 hover:border-b border-black hover:text-black">
-              خانه
-            </li>
-          </Link>
-        </ul>
+        <Link to={"/"}>
+          <h1 className="font-bold text-lg xs:text-2xl md:text-3xl flex flex-wrap">
+            <span className="text-slate-700">Estate</span>
+            <span className="text-slate-500">Alireza</span>
+          </h1>
+        </Link>
 
         {/* mobile menu */}
 
         <ul className="sm:hidden  border-b-transparent border-[1px] xs:gap-12 gap-8 items-center justify-center w-full py-5 text-lg xs:text-xl bg-slate-200 flex fixed bottom-0 right-0 rounded-tr-full rounded-tl-full shadow-[0_25px_50px_15px_rgba(0,0,0,.4)]">
-          <Link to={"/sign-in"}>
-            <li className="text-slate-700 hover:text-black duration-300 flex gap-1 items-center">
-                <FaUser />
-              ورود
-            </li>
-          </Link>
-          <Link to={"/about"}>
-            {" "}
-            <li className="text-slate-700 hover:text-black duration-300 flex  items-center gap-1">
-            <FaLayerGroup />
-              درباره
-            </li>
-          </Link>
           <Link to={"/"}>
             <li className="text-slate-700 hover:text-black duration-300 flex  gap-1">
               <FaHome />
               خانه
             </li>
+          </Link>
+          <Link to={"/about"}>
+            {" "}
+            <li className="text-slate-700 hover:text-black duration-300 flex  items-center gap-1">
+              <FaLayerGroup />
+              درباره
+            </li>
+          </Link>
+
+          <Link to={"/profile"}>
+            {currentUser ? (
+              <img
+                src={currentUser.avatar}
+                className="rounded-full h-7 w-7 object-cover"
+              />
+            ) : (
+              <li className="text-slate-700 hover:text-black duration-300 flex gap-1 items-center">
+                <FaUser/>
+                ورود
+              </li>
+            )}
           </Link>
         </ul>
 
@@ -61,12 +56,31 @@ export default function Navbar() {
           <FaSearch />
         </form>
 
-        <Link to={"/"}>
-          <h1 className="font-bold text-lg xs:text-2xl md:text-3xl flex flex-wrap">
-            <span className="text-slate-700">Estate</span>
-            <span className="text-slate-500">Alireza</span>
-          </h1>
-        </Link>
+        <ul className="sm:flex gap-12 items-center hidden text-lg">
+          <Link to={"/"}>
+            <li className="text-slate-700 hover:border-b border-black hover:text-black">
+              خانه
+            </li>
+          </Link>
+          <Link to={"/about"}>
+            {" "}
+            <li className="text-slate-700 hover:border-b border-black hover:text-black">
+              درباره
+            </li>
+          </Link>
+          <Link to={"/profile"}>
+            {currentUser ? (
+              <img
+                src={currentUser.avatar}
+                className="rounded-full h-7 w-7 object-cover"
+              />
+            ) : (
+              <li className="text-slate-700 hover:border-b border-black hover:text-black">
+                ورود
+              </li>
+            )}
+          </Link>
+        </ul>
       </div>
     </header>
   );
