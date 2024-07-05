9f47b1e89ecfeb941d522f0e55c0930dc3e20cab
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2024-07-05 21:47:47 +0430

    complete update user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {
   getDownloadURL,
   getStorage,
@@ -7,16 +7,23 @@ import {
   uploadBytesResumable,
 } from "firebase/storage";
 import { app } from "../firebase";
+import {
+  updateUserFailure,
+  updateUserStart,
+  updateUserSuccess,
+} from "../redux/user/userSlice";
+import { PulseLoader } from "react-spinners";
 
 export default function Profile() {
   const fileRef = useRef(null);
-  const { currentUser } = useSelector((state) => state.user);
+  const { currentUser, loading, error } = useSelector((state) => state.user);
   const [file, setFile] = useState(undefined);
   const [filePerc, setFilePerc] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
+  const [updateSuccess, setUpdateSuccess] = useState(false);
 
-  console.log(formData);
+  const dispatch = useDispatch();
 
   const handleFileUpload = (file) => {
     const storage = getStorage(app);
@@ -48,10 +55,38 @@ export default function Profile() {
     }
   }, [file]);
 
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.error) {
+        dispatch(updateUserFailure(data.error));
+        return;
+      }
+
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">پروفایل</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           onChange={(e) => setFile(e.target.files[0])}
           type="file"
@@ -61,44 +96,55 @@ export default function Profile() {
         <img
           accept="image/*"
           onClick={() => fileRef.current.click()}
-          src={formData.avatar || currentUser.avatar}
+          src={formData?.avatar || currentUser.avatar}
           alt="profile"
           className="rounded-full h-24 w-24 object-cover cursor-pointer self-center my-2 border border-slate-300"
         />
         <p className="text-sm self-center">
           {fileUploadError ? (
-            <span className="text-red-700">  (حجم تصویر کمتر از دو مگابایت باشد) بارگذاری با خطا مواجه شد </span>
+            <span className="text-red-700">
+              {" "}
+              (حجم تصویر کمتر از دو مگابایت باشد) بارگذاری با خطا مواجه شد{" "}
+            </span>
           ) : filePerc > 0 && filePerc < 100 ? (
             <span className="text-slate-700">{`بارگذاری ${filePerc}%`}</span>
-          ) : (filePerc === 100 ) ? (
+          ) : filePerc === 100 ? (
             <span className="text-green-700">تصویر با موفقیت بارگذاری شد</span>
           ) : (
             ""
           )}
         </p>
         <input
+          onChange={handleChange}
+          defaultValue={currentUser.email}
           type="text"
           placeholder="ایمیل"
-          id="username"
-          className="border p-3 rounded-lg"
-        />
-
-        <input
-          type="text"
-          placeholder="نام کاربری"
           id="email"
           className="border p-3 rounded-lg"
         />
 
         <input
+          onChange={handleChange}
+          defaultValue={currentUser.username}
           type="text"
+          placeholder="نام کاربری"
+          id="username"
+          className="border p-3 rounded-lg"
+        />
+
+        <input
+          onChange={handleChange}
+          type="password"
           placeholder="گذرواژه"
           id="password"
           className="border p-3 rounded-lg"
         />
 
-        <button className="bg-slate-700 text-white rounded-lg p-3 hover:opacity-95 disabled:opacity-80">
-          بروز رسانی
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white rounded-lg p-3 hover:opacity-95 disabled:opacity-80 flex items-center justify-center min-h-12"
+        >
+          {loading ? <PulseLoader size={10} /> : "بروز رسانی"}
         </button>
       </form>
 
@@ -106,6 +152,14 @@ export default function Profile() {
         <span className="text-red-700 cursor-pointer">پاک کردن حساب</span>
         <span className="text-slate-700 cursor-pointer">خروج از حساب</span>
       </div>
+
+      <p className="text-red-700 mt-5">{error && error}</p>
+      <p className="text-green-700 mt-5">
+        {updateSuccess &&
+        (formData.email || formData.username || formData.password)
+          ? "اطلاعات بروز رسانی شد"
+          : ""}
+      </p>
     </div>
   );
 }
