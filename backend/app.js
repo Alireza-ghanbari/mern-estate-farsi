f3fdf3f8ce3da1659a6d5280bce1af100acc36a8
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2024-07-05 20:53:29 +0430

    create user update api

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,17 @@ import express from "express"
 import morgan from "morgan"
 
 import authRouter from "./routes/auth.route.js"
+import userRouter from "./routes/user.route.js"
+import cookieParser from "cookie-parser"
 
 const app = express()
 
 app.use(morgan("dev"))
 app.use(express.json())
+app.use(cookieParser())
 
 // Routes
-// app.use("/api/user", userRouter)
+app.use("/api/user", userRouter)
 app.use("/api/auth", authRouter)
 
 app.use((err, req, res, next) => {
---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,47 @@
+import bcryptjs from "bcryptjs";
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+
+export const updateUser = async (req, res, next) => {
+    const {username, password, email } = req.body
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "فقط حساب خود را اپدیت کنید"));
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (email) {
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ error: "ایمبل معتبر نمیباشد" });
+    }
+  }
+
+  try {
+    if (password) {
+      if (password.length < 4) {
+        return res.status(400).json({
+          error: "گذرواژه باید بیشتر از 4 حرف داشته باشد",
+        });
+      } else {
+        req.body.password = bcryptjs.hashSync(req.body.password, 10);
+      }
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
+
+    const { password: pw, ...userInfo } = updatedUser._doc;
+
+    res.status(200).json(userInfo);
+  } catch (error) {
+    next(error);
+  }
+};
