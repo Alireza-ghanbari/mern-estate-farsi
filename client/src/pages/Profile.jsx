c3b6c6a79fee85dc61a3e1dcfcd025022062c1f1
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2024-07-14 22:08:01 +0430

    Update Profile.jsx

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -214,6 +214,7 @@ export default function Profile() {
           ? "اطلاعات بروز رسانی شد"
           : ""}
       </p>
+      <button className="text-green-700">نمایش آگهی ها</button>
     </div>
   );
 }
