74efbf9959060963079bdd4329fbdf753d6f25fd
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2024-07-18 11:44:19 +0430

    add swiper in listing page

--- client/src/pages/ListingPage.jsx
+++ client/src/pages/ListingPage.jsx
@@ -1,7 +1,65 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { PulseLoader } from "react-spinners";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore from "swiper";
+import { Navigation } from "swiper/modules";
+import "swiper/css/bundle";
 
 export default function ListingPage() {
+  SwiperCore.use([Navigation]);
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const params = useParams();
+  useEffect(() => {
+    const fetchListing = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(`/api/listing/get/${params.listingId}`);
+        const data = await res.json();
+        if (!res.ok) {
+          setError(true);
+          setLoading(false);
+        }
+        setListing(data);
+        setLoading(false);
+        setError(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchListing();
+  }, [params.listingId]);
   return (
-    <div>ListingPage</div>
-  )
+    <main>
+      {loading && (
+        <div className="flex justify-center items-center mt-32">
+          <PulseLoader size={15} />
+        </div>
+      )}
+      {error && (
+        <p className=" text-center my-20 text-2xl">خطا در گرفتن اطلاعات !</p>
+      )}
+
+      {listing && !loading && !error && (
+        <div>
+          <Swiper navigation>
+            {listing.imageUrls.map((url) => (
+              <SwiperSlide key={url}>
+                <div
+                  className="h-[550px]"
+                  style={{
+                    background: `url(${url}) center no-repeat`,
+                    backgroundSize: "cover",
+                  }}
+                ></div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      )}
+    </main>
+  );
 }
