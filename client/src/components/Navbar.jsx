febd539c36fa61752b6aa91374909cddc1eade95
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2024-07-20 12:14:57 +0430

    complete search form

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,31 @@
 import { FaHome, FaSearch, FaUser } from "react-icons/fa";
 import { FaLayerGroup } from "react-icons/fa6";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
+  const [searchTerm, setSearchTerm] = useState("");
   const { currentUser } = useSelector((state) => state.user);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermForUrl = urlParams.get("searchTerm");
+    if (searchTermForUrl) {
+      setSearchTerm(searchTermForUrl);
+    }
+  }, [location.search]);
+
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-7xl mx-auto py-3 px-2 sm:px-8">
@@ -40,20 +61,27 @@ export default function Navbar() {
               />
             ) : (
               <li className="text-slate-700 hover:text-black duration-300 flex gap-1 items-center">
-                <FaUser/>
+                <FaUser />
                 ورود
               </li>
             )}
           </Link>
         </ul>
 
-        <form className="bg-slate-100 px-3 py-2 rounded-lg flex items-center">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-slate-100 px-3 py-2 rounded-lg flex items-center"
+        >
           <input
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
             type="text"
             placeholder="جستوجو ..."
             className="bg-transparent focus:outline-none w-20 xs:w-32 sm:w-32 md:w-44 lg:w-64"
           />
-          <FaSearch />
+          <button>
+            <FaSearch />
+          </button>
         </form>
 
         <ul className="sm:flex gap-12 items-center hidden text-lg">
