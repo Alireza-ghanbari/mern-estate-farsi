c822aa5bddf34feaa92cfb6ff3ada4b0f3515aec
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2024-07-21 13:17:32 +0430

    add onSubmit to search page

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,36 +1,169 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Search() {
+  const navigate = useNavigate();
+  const [sidebarData, setSidebarData] = useState({
+    searchTerm: "",
+    type: "all",
+    parking: false,
+    furnished: false,
+    offer: false,
+    sort: "created_at",
+    order: "desc",
+  });
+  const [loading, setLoading] = useState(false);
+  const [listings, setListings] = useState([]);
+  console.log(listings)
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    const typeFromUrl = urlParams.get("type");
+    const parkingFromUrl = urlParams.get("parking");
+    const furnishedFromUrl = urlParams.get("furnished");
+    const offerFromUrl = urlParams.get("offer");
+    const sortFromUrl = urlParams.get("sort");
+    const orderFromUrl = urlParams.get("order");
+
+    if (
+      searchTermFromUrl ||
+      typeFromUrl ||
+      parkingFromUrl ||
+      furnishedFromUrl ||
+      offerFromUrl ||
+      sortFromUrl ||
+      orderFromUrl
+    ) {
+      setSidebarData({
+        searchTerm: searchTermFromUrl || "",
+        type: typeFromUrl || "all",
+        parking: parkingFromUrl === "true" ? true : false,
+        furnished: furnishedFromUrl === "true" ? true : false,
+        offer: offerFromUrl === "true" ? true : false,
+        sort: sortFromUrl || "created_at",
+        order: orderFromUrl || "desc",
+      });
+    }
+
+    const fetchListings = async () => {
+        setLoading(true)
+        const searchQuery = urlParams.toString()
+        const res = await fetch(`/api/listing/get?${searchQuery}`)
+        const data = await res.json()
+        setListings(data)
+        setLoading(false)
+    };
+
+    fetchListings();
+  }, [location.search]);
+
+  const handleChange = (e) => {
+    if (
+      e.target.id === "all" ||
+      e.target.id === "rent" ||
+      e.target.id === "sale"
+    ) {
+      setSidebarData({ ...sidebarData, type: e.target.id });
+    }
+
+    if (e.target.id === "searchTerm") {
+      setSidebarData({ ...sidebarData, searchTerm: e.target.value });
+    }
+
+    if (
+      e.target.id === "parking" ||
+      e.target.id === "furnished" ||
+      e.target.id === "offer"
+    ) {
+      setSidebarData({
+        ...sidebarData,
+        [e.target.id]:
+          e.target.checked || e.target.checked === "true" ? true : false,
+      });
+    }
+
+    if (e.target.id === "sort_order") {
+      const sort = e.target.value.split("_")[0] || "created_at";
+
+      const order = e.target.value.split("_")[1] || "desc";
+
+      setSidebarData({ ...sidebarData, sort, order });
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams();
+    urlParams.set("searchTerm", sidebarData.searchTerm);
+    urlParams.set("type", sidebarData.type);
+    urlParams.set("parking", sidebarData.parking);
+    urlParams.set("furnished", sidebarData.furnished);
+    urlParams.set("offer", sidebarData.offer);
+    urlParams.set("sort", sidebarData.sort);
+    urlParams.set("order", sidebarData.order);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
   return (
     <div className="flex flex-col lg:flex-row">
       <div className=" p-3 xs:p-7 border-b-2 lg:border-r-2 lg:min-h-screen">
-        <form className="flex flex-col gap-8">
+        <form className="flex flex-col gap-8" onSubmit={handleSubmit}>
           <div className="flex items-center gap-2">
-            <label className="whitespace-nowrap font-semibold">نام آگهی :</label>
+            <label className="whitespace-nowrap font-semibold">
+              نام آگهی :
+            </label>
             <input
               type="text"
               id="searchTerm"
               placeholder="جستوجو کنید"
               className="border rounded-lg p-3 w-full whitespace-nowrap"
+              value={sidebarData.searchTerm}
+              onChange={handleChange}
             />
           </div>
           <div className="flex gap-3 flex-wrap items-center">
             <label className="font-semibold">نوع آگهی :</label>
             <div className="flex gap-2">
               <span>فروش و اجاره</span>
-              <input type="checkbox" id="all" className="w-5" />
+              <input
+                type="checkbox"
+                id="all"
+                className="w-5"
+                checked={sidebarData.type === "all"}
+                onChange={handleChange}
+              />
             </div>
             <div className="flex gap-2">
               <span>اجاره</span>
-              <input type="checkbox" id="rent" className="w-5" />
+              <input
+                type="checkbox"
+                id="rent"
+                className="w-5"
+                checked={sidebarData.type === "rent"}
+                onChange={handleChange}
+              />
             </div>
             <div className="flex gap-2">
               <span>فروش</span>
-              <input type="checkbox" id="sale" className="w-5" />
+              <input
+                type="checkbox"
+                id="sale"
+                className="w-5"
+                checked={sidebarData.type === "sale"}
+                onChange={handleChange}
+              />
             </div>
             <div className="flex gap-2">
               <span>تخفیف دار</span>
-              <input type="checkbox" id="offer" className="w-5" />
+              <input
+                type="checkbox"
+                id="offer"
+                className="w-5"
+                checked={sidebarData.offer}
+                onChange={handleChange}
+              />
             </div>
           </div>
 
@@ -38,21 +171,38 @@ export default function Search() {
             <label className="font-semibold">امکانات :</label>
             <div className="flex gap-2">
               <span>پارکینگ</span>
-              <input type="checkbox" id="parking" className="w-5" />
+              <input
+                type="checkbox"
+                id="parking"
+                className="w-5"
+                checked={sidebarData.parking}
+                onChange={handleChange}
+              />
             </div>
             <div className="flex gap-2">
               <span>مبله</span>
-              <input type="checkbox" id="furnished" className="w-5" />
+              <input
+                type="checkbox"
+                id="furnished"
+                className="w-5"
+                checked={sidebarData.furnished}
+                onChange={handleChange}
+              />
             </div>
           </div>
 
           <div className="flex items-center gap-2">
             <label className="font-semibold">مرتب سازی :</label>
-            <select id="sort_order" className="border rounded-lg px-2 py-1">
-              <option>بیشترین قیمت</option>
-              <option>کمترین قیمت</option>
-              <option>جدید ترین</option>
-              <option>قدیمی ترین</option>
+            <select
+              id="sort_order"
+              className="border rounded-lg px-2 py-1"
+              onChange={handleChange}
+              defaultValue={"created_at_desc"}
+            >
+              <option value="regularPrice_desc">بیشترین قیمت</option>
+              <option value="regularPrice_asc">کمترین قیمت</option>
+              <option value="createdAt_desc">جدید ترین</option>
+              <option value="createdAt_asc">قدیمی ترین</option>
             </select>
           </div>
 
@@ -61,8 +211,11 @@ export default function Search() {
           </button>
         </form>
       </div>
+
       <div className="">
-        <h1 className="text-2xl font-semibold border-b p-3 text-slate-700 lg:mt-5">نتایج :</h1>
+        <h1 className="text-2xl font-semibold border-b p-3 text-slate-700 lg:mt-5">
+          نتایج :
+        </h1>
       </div>
     </div>
   );
